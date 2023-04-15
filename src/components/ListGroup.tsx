6e9ded32602a0a46eec758d4d89ad619b1a376ca
mithun thambawita <mithunthambavita@gmail.com>
2023-04-15 08:12:45 +0530

    conditioning rendering in list group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>} //test in console true &&
+      'mithun' = 'Mithun'
       <ul className="list-group">
         {items.map((item) => (
-          <li>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
