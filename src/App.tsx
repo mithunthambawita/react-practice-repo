0a8b6faa3d9f953eea690938ff7968cc2be8db91
mithun thambawita <mithunthambavita@gmail.com>
2023-04-16 22:48:53 +0530

    passing function via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import ListGroup from "./components/ListGroup";
 let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
+const handleSelecItem = (item: string) => {
+  console.log(item);
+};
 function App() {
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"}></ListGroup>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelecItem}
+      ></ListGroup>
     </div>
   );
 }
